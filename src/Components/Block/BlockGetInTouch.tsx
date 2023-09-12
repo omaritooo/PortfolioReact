cf580bc99f5edb79b7ca1fbc5c3a9e7237ff4b80
omaritooo <48407184+omaritooo@users.noreply.github.com>
2023-09-12 18:13:57 +0300

    Update BlockGetInTouch.tsx

--- src/Components/Block/BlockGetInTouch.tsx
+++ src/Components/Block/BlockGetInTouch.tsx
@@ -11,7 +11,7 @@ export const BlockGetInTouch = () => {
           Get In Touch
         </h2>
         <p className="text-xl font-raleway text-center text-light-main dark:text-white font-light lg:max-w-[50%] mt-4">
-          Although I’m not currently looking for any new opportunities, my inbox is always open.
+         I am actively looking for new work opportunities, whether it's full time or contractor work, my inbox is always open.
           Whether you have a question or just want to say hi, I’ll try my best to get back to you!
         </p>
         <a
